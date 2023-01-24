149a7aec55d5643f5ab1c4c6299ccce7570041e1
Fredrik Gustafsson <freddish@hotmail.com>
2023-01-24 12:50:07 +0000

    add code for intro, scene 1-3

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+Welcome to my adventure game.
+Hope you will enjoy it!
+To check out more visit www.github.com/codingbyfreddish
+"""
+
+
+def run_game():
+    print("Hey there! First things first, what is your name?")
+    name = input("Name: ")
+    print(f"{name}? Well, i've heard worse... kind of.")
+    print(f"So {name} are you ready for an adventure? 'Yes' or 'No'")
+    ready = input("Answer: ")
+
+
+
+
+
+
+
+run_game()
